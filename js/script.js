e9fe0f06a97ec13062b79533b47632613da8d49b
Ольга Головко <66256500+OlgaGolovko@users.noreply.github.com>
2022-01-26 21:53:31 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // new Swiper(".swiper-container");
 
-let swiper = new Swiper(".swiper-container", {
+// let swiper = new Swiper(".swiper-container", {
 
 
-    direction: "horizontal",
-    // loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    }
+//     direction: "horizontal",
+//     // loop: true,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev"
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     }
 
-});
+// });
